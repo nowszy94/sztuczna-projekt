825d8fa44b3a40bec612350a61b67eddaa4259c0
Szymon Nowak <szymon.nowak@allegro.pl>
2019-06-15 13:26:28 +0200

    analyze of result data

--- analyzer.js
+++ analyzer.js
@@ -7,15 +7,22 @@ function analyze(resultsPath, toTrainPath) {
     fs.readFile(resultsPath, 'utf8', (_, resultsContent) => {
         const resultsAsArray = resultsContent.split('\r\n')
             .map(item => item.substr(0, 4));
-
+        let sum = 0;
+        let count = 0;
         fs.readFile(toTrainPath, 'utf8', (_, toTrainContent) => {
             const toTrainAsArray = toTrainContent.split('\n')
                 .map(e => e.substr(0, 1));
             for (let i = 0; i < resultsAsArray.length - 1 /* last empty line */; i++) {
                 const scoreFromVW = resultsAsArray[i];
                 const originScore = toTrainAsArray[i];
-                fs.appendFile(analyzeResultPath, `${originScore} | ${scoreFromVW}\n`, () => {});
+                const diff = (parseFloat(scoreFromVW) - parseFloat(originScore)) / 2;
+                sum += diff >= 0 ? diff : diff * (-1);
+                count ++;
+
+                fs.appendFile(analyzeResultPath, `${originScore} | ${scoreFromVW}\n`, 'utf8', () => {});
             }
+            const avg = sum / count;
+            setTimeout(function() {fs.appendFile(analyzeResultPath, `Wynik: ${avg.toFixed(2)}`, 'utf8', () => {})}, 1000)
         });
     });
 }
